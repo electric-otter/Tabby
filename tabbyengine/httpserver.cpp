276bb86933c08ca5dcd9806848ba64258723cd19
electric-otter <krw122081@gmail.com>
2024-12-29 20:53:22 +0000

    Create httpserver.cpp

---
+++ tabbyengine/httpserver.cpp
@@ -0,0 +1,10 @@
+#ifndef INCLUDED_HTTP_TCPSERVER_LINUX
+#define INCLUDED_HTTP_TCPSERVER_LINUX
+namespace http
+{    class TcpServer
+    {
+    public:
+        TcpServer();
+        ~TcpServer();    private:
+        
+    };} // namespace http#endif
